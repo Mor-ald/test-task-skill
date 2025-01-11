2bc71383b6f39ac97a3ce9694a836d31179558e8
Mor-ald <mor.ald@yandex.ru>
2025-01-12 00:12:13 +0300

    fix(components): remove case for 2, add case for 0 in TypeCell

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -46,15 +46,7 @@ const Table: FC<ITable> = observer(({ callsApiData, callType }) => {
 
 	const TypeCell = useCallback(({ item }: { item: TableItem }) => {
 		switch (item.in_out) {
-			case 1:
-				return (
-					<td>
-						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M18.5217 7.17704L17.3447 6L7.66957 15.6751V10.1739H6V18.5217H14.3478V16.8522H8.84661L18.5217 7.17704Z" fill="#002CFB" />
-						</svg>
-					</td>
-				);
-			case 2:
+			case 0:
 				return (
 					<td>
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -62,6 +54,14 @@ const Table: FC<ITable> = observer(({ callsApiData, callType }) => {
 								d="M6.00023 17.3447L7.17728 18.5217L16.8524 8.8466V14.3478H18.522V5.99999L10.1741 5.99999V7.66955L15.6754 7.66955L6.00023 17.3447Z"
 								fill="#28A879"
 							/>
+						</svg>
+					</td>
+				);
+			case 1:
+				return (
+					<td>
+						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M18.5217 7.17704L17.3447 6L7.66957 15.6751V10.1739H6V18.5217H14.3478V16.8522H8.84661L18.5217 7.17704Z" fill="#002CFB" />
 						</svg>
 					</td>
 				);
