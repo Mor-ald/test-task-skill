90f5ad899118a6655b58f1b3dfc614329be0cdcb
Mor-ald <mor.ald@yandex.ru>
2025-01-12 14:56:14 +0300

    feature(components): add ActionButton

--- src/components/type-selector/TypeSelector.tsx
+++ src/components/type-selector/TypeSelector.tsx
@@ -6,6 +6,7 @@ import { FC, useCallback, useEffect, useRef, useState } from "react";
 import TypeSelectorStore from "./TypeSelectorStore.ts";
 import { toJS } from "mobx";
 import { CallTypes } from "../../ts/CallTypes.ts";
+import ActionButton from "../action-button/ActionButton.tsx";
 
 /**
  * TypeSelector component
@@ -30,58 +31,21 @@ const TypeSelector: FC<ITypeSelector> = observer(({ value, onChangeValue }) => {
 
 	// Current text of selector title
 	const SelectTitleText = useCallback(() => {
-		return (
-			<span
-				className={styles[value === "all" ? "select-selected" : "select-selected-active"]}
-				onClick={() => store.onChangeFocus(!toJS(store.isFocused))}
-			>
-				{toJS(store.options)[value]}
-			</span>
-		);
+		return <span className={styles[value === "all" ? "select-selected" : "select-selected-active"]}>{toJS(store.options)[value]}</span>;
 	}, [store, value]);
-
-	// Current arrow of selector
-	const SelectArrow = useCallback(() => {
-		return store.isFocused ? (
-			<div className={styles["select-arrow"]} onClick={() => store.onChangeFocus(!toJS(store.isFocused))}>
-				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<g clipPath="url(#clip0_1_866)">
-						<path d="M12 8L6 14L7.41 15.41L12 10.83L16.59 15.41L18 14L12 8Z" fill="#002CFB" />
-					</g>
-					<defs>
-						<clipPath id="clip0_1_866">
-							<rect width="24" height="24" fill="white" />
-						</clipPath>
-					</defs>
-				</svg>
-			</div>
-		) : (
-			<div className={styles["select-arrow"]} onClick={() => store.onChangeFocus(!toJS(store.isFocused))}>
-				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<g clipPath="url(#clip0_1_863)">
-						<path opacity="0.8" d="M7.41 8.58997L12 13.17L16.59 8.58997L18 9.99997L12 16L6 9.99997L7.41 8.58997Z" fill="#ADBFDF" />
-					</g>
-					<defs>
-						<clipPath id="clip0_1_863">
-							<rect width="24" height="24" fill="white" />
-						</clipPath>
-					</defs>
-				</svg>
-			</div>
-		);
-	}, [store]);
 
 	// Title of selector
 	const SelectTitle = useCallback(() => {
 		return (
-			<span ref={titleRef}>
-				<div className={styles["select-title"]}>
-					<SelectTitleText />
-					<SelectArrow />
+			<span ref={titleRef} onClick={() => store.onChangeFocus(!toJS(store.isFocused))}>
+				<div>
+					<ActionButton active={toJS(store.isFocused)}>
+						<SelectTitleText />
+					</ActionButton>
 				</div>
 			</span>
 		);
-	}, [SelectArrow, SelectTitleText]);
+	}, [SelectTitleText, store]);
 
 	// DropDown of selector
 	const SelectDropDown = useCallback(() => {
