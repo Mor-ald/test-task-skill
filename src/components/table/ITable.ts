f34f1ac891f21acb6ee77c244d6e98a298f35500
Mor-ald <mor.ald@yandex.ru>
2025-01-12 16:09:11 +0300

    feature(components): add new fields
    add sortByDate, sortByDuration, onToggleSortByDate, onToggleSortByDuration

--- src/components/table/ITable.ts
+++ src/components/table/ITable.ts
@@ -7,4 +7,8 @@ import { CallTypes } from "../../ts/CallTypes";
 export interface ITable {
 	callsApiData: CallsApiData;
 	callType: CallTypes;
+	sortByDate: boolean;
+	sortByDuration: boolean;
+	onToggleSortByDate(): void;
+	onToggleSortByDuration(): void;
 }
